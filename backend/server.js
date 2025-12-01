0b2bb029c677e525100a856bd04bb37b065f2b79
ruchithata <ruchitha.ta83@gmail.com>
2025-12-01 19:22:38 +0530

    updated vite config + backend url

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/database/db.js';
 import productRoutes from './src/routes/productRoutes.js';
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,14 @@ const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",          // for local dev
+    "https://mern-stack-7k4g.vercel.app/" // your Vercel URL
+  ],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
